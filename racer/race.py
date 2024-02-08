30a0ee2dacff3f7da6d885a49409776c753211d4
BenAbbott25 <benabbott250@gmail.com>
2024-02-08 00:20:46 +0000

    Update frame dimensions and add driver class

--- racer/race.py
+++ racer/race.py
@@ -1,8 +1,8 @@
 import pygame
 import numpy as np
 
-frames_x = 1080
-frames_y = 720
+frames_x = 1440
+frames_y = 900
 
 class Game:
     def __init__(self, frames_x, frames_y):
@@ -10,25 +10,37 @@ class Game:
         self.frames_y = frames_y
 
         self.course = Course(frames_x, frames_y)
+        self.start = (self.course.points[0][0], self.course.points[0][1])
+        self.driver = Driver(self.start[0], self.start[1], self.course.checkpoints[0].angle)
+        
 
     def draw(self, screen):
         self.course.draw(screen)
+        self.racer.draw(screen)
 
 class Checkpoint:
-    def __init__(self, index, position, angle):
+    def __init__(self, index, position, angle, angle_derivative):
         self.index = index
         self.position = position
         self.angle = angle
-        self.width = 10
+        self.angle_derivative = angle_derivative
+        self.width = min(max(angle_derivative, 10), 50)
+        print(f"Index: {self.index},Width: {self.width}, Angle Derivative: {self.angle_derivative}")
         self.left_position = (self.position[0] + np.cos(self.angle + np.pi/2)*self.width, self.position[1] + np.sin(self.angle + np.pi/2)*self.width)
         self.right_position = (self.position[0] + np.cos(self.angle - np.pi/2)*self.width, self.position[1] + np.sin(self.angle - np.pi/2)*self.width)
 
     def draw(self, screen):
-        end_position = (self.position[0] + np.cos(self.angle)*50, self.position[1] + np.sin(self.angle)*50)
-        pygame.draw.line(screen, (255, 0, 0), self.position, end_position)
-        pygame.draw.line(screen, (0, 255, 0), self.left_position, self.right_position)
+        color = (255, 0, 0) if self.index == 0 else (0, 255, 0)
+        if self.index % 10 == 0:
+            pygame.draw.line(screen, color, self.left_position, self.right_position)
+            yellow = (255, 255, 0)
+            end_mid = (self.position[0] + np.cos(self.angle)*15, self.position[1] + np.sin(self.angle)*15)
+            end_left = (self.position[0] + np.cos(self.angle + np.pi/8)*10, self.position[1] + np.sin(self.angle + np.pi/8)*10)
+            end_right = (self.position[0] + np.cos(self.angle - np.pi/8)*10, self.position[1] + np.sin(self.angle - np.pi/8)*10)
+            pygame.draw.line(screen, yellow, self.position, end_mid)
+            pygame.draw.line(screen, yellow, end_mid, end_left)
+            pygame.draw.line(screen, yellow, end_mid, end_right)
         
-
 class Course:
     def __init__(self, frames_x, frames_y):
         self.frames_x = frames_x
@@ -36,36 +48,54 @@ class Course:
 
         t = 0
         self.points = []
-        while t < 2.1 * np.pi:
-            x = 300 * np.cos(t) + frames_x / 2
-            y = 300 * np.sin(2*t) + frames_y / 2
+        while t <= 2 * np.pi:
+            x = 500 * np.cos(t) + frames_x / 2
+            y = 250 * np.sin(2*t) + frames_y / 2
             self.points.append([x, y])
-            t += 0.1 * np.pi
+            t += 0.005 * np.pi
 
         self.checkpoints = []
         for i in range(len(self.points) - 1):
             dx = self.points[i+1][0] - self.points[i][0]
             dy = self.points[i+1][1] - self.points[i][1]
             angle = np.arctan2(dy, dx)
-            self.checkpoints.append(Checkpoint(i, self.points[i], angle))
+            
+            previous_dx = self.points[i][0] - self.points[i-1][0]
+            previous_dy = self.points[i][1] - self.points[i-1][1]
+            angle0 = np.arctan2(previous_dy, previous_dx)
+            angle_derivative = np.sin(abs(angle0- angle)) * 1000
+            
+            self.checkpoints.append(Checkpoint(i, self.points[i], angle, angle_derivative))
 
-        for checkpoint in self.checkpoints:
-            print(f"Index: {checkpoint.index}, Position: {checkpoint.position}, Angle: {checkpoint.angle}")
+        # smooth width changes
+        for i in range(len(self.checkpoints)-1):
+            if self.checkpoints[i].width < self.checkpoints[i-1].width and self.checkpoints[i].width < self.checkpoints[i+1].width:
+                self.checkpoints[i].width = (self.checkpoints[i-1].width + self.checkpoints[i+1].width) / 2
+                self.checkpoints[i].left_position = (self.checkpoints[i].position[0] + np.cos(self.checkpoints[i].angle + np.pi/2)*self.checkpoints[i].width, self.checkpoints[i].position[1] + np.sin(self.checkpoints[i].angle + np.pi/2)*self.checkpoints[i].width)
+                self.checkpoints[i].right_position = (self.checkpoints[i].position[0] + np.cos(self.checkpoints[i].angle - np.pi/2)*self.checkpoints[i].width, self.checkpoints[i].position[1] + np.sin(self.checkpoints[i].angle - np.pi/2)*self.checkpoints[i].width)
 
         
 
     def draw(self, screen):
-        for i in range(len(self.points) - 1):
-            pygame.draw.line(screen, (255, 255, 255), self.points[i], self.points[i+1])
-
         for checkpoint in self.checkpoints:
             checkpoint.draw(screen)
         
-        for i in range(len(self.checkpoints) - 1):
-            pygame.draw.line(screen, (255, 255, 255), self.checkpoints[i].left_position, self.checkpoints[i+1].left_position)
-            pygame.draw.line(screen, (255, 255, 255), self.checkpoints[i].right_position, self.checkpoints[i+1].right_position)
-        pygame.draw.line(screen, (255, 255, 255), self.checkpoints[-1].left_position, self.checkpoints[0].left_position)
-        pygame.draw.line(screen, (255, 255, 255), self.checkpoints[-1].right_position, self.checkpoints[0].right_position)
+        for i in range(len(self.checkpoints)):
+            pygame.draw.line(screen, (255, 255, 255), self.checkpoints[i-1].left_position, self.checkpoints[i].left_position)
+            pygame.draw.line(screen, (255, 255, 255), self.checkpoints[i-1].right_position, self.checkpoints[i].right_position)
+
+class Driver:
+    def __init__(self, x, y, angle):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.front_left = (self.x + np.cos(self.angle + np.pi/6)*10, self.y + np.sin(self.angle + np.pi/6)*10)
+        self.front_right = (self.x + np.cos(self.angle - np.pi/6)*10, self.y + np.sin(self.angle - np.pi/6)*10)
+        self.back_left = (self.x + np.cos(self.angle + np.pi + np.pi/6)*10, self.y + np.sin(self.angle + np.pi + np.pi/6)*10)
+        self.back_right = (self.x + np.cos(self.angle + np.pi - np.pi/6)*10, self.y + np.sin(self.angle + np.pi - np.pi/6)*10)
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), [self.front_left, self.front_right, self.back_left, self.back_right])
 
 def main():
     pygame.init()
@@ -80,7 +110,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        screen.fill((0, 0, 0))
+        screen.fill((105, 105, 105))
         game.draw(screen)
         pygame.display.flip()
 
